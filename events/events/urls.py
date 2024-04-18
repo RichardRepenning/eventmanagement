1cc870e19b8a7e3e50e951a35ae7ecc8167bcb86
RichardRepenning <richardrepenning@gmail.com>
2024-04-18 22:22:51 +0200

    added bootstrap 5.3 to project

--- events/events/urls.py
+++ events/events/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
+from django.conf import settings
 from ninja import NinjaAPI
 
 api = NinjaAPI()
@@ -28,4 +30,5 @@ urlpatterns = [
     path('', include('ticketApp.urls')),
     path('admin/', admin.site.urls),
     path("api/", api.urls),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
