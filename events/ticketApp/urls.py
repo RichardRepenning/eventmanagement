069bfbe97a2fb21c1dbeb4a022e90c14fd50a08d
RichardRepenning <richardrepenning@gmail.com>
2024-04-22 16:57:13 +0200

    removed django-ninja from project, because too much effort for the short time

--- events/ticketApp/urls.py
+++ events/ticketApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.main, name='main'),
+  path('', views.main, name='main'),
   path("events/", views.events, name="events"),
   path('testing/', views.testing, name='testing')
 ]
