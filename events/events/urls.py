2110b301c9f99600a6cbc98e59b6b7268d089aa7
RichardRepenning <richardrepenning@gmail.com>
2024-04-24 12:14:05 +0200

    added auth urls to project

--- events/events/urls.py
+++ events/events/urls.py
@@ -6,5 +6,6 @@ from django.conf import settings
 urlpatterns = [
     path('', include('ticketApp.urls')),
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
